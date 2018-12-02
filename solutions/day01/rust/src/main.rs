6a34f525a71fec16cb8181b360bac9156071007a
KrzaQ <krzaq@krzaq.cc>
2018-12-02 23:47:17 +0100

    Prettify as per @Xorlev's suggestions.

--- solutions/day01/rust/src/main.rs
+++ solutions/day01/rust/src/main.rs
@@ -1,25 +1,24 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io;
+use std::io::BufReader;
 use std::io::prelude::*;
-use std::collections::HashSet;
 
-fn read_file(name: &str) -> Result<String, io::Error>
+fn read_file(name: &str) -> Result<Vec<String>, io::Error>
 {
-    let mut file = File::open(name)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-
-    return Ok(contents);
+    let file = File::open(name)?;
+    let buf_reader = BufReader::new(file);
+    return buf_reader.lines().collect();
 }
 
 fn main() -> Result<(), io::Error>
 {
-    let data = read_file("../data.txt")?;
-    let data = data.split('\n');
-    let data = data.filter_map(|l| l.parse::<i32>().ok());
-    let data = data.collect::<Vec<i32>>();
+    let data = read_file("../data.txt")?
+        .iter()
+        .filter_map(|l| l.parse::<i32>().ok())
+        .collect::<Vec<i32>>();
 
-    let part1 = data.iter().fold(0, |t, e| t + e);
+    let part1 = data.iter().sum::<i32>();
 
     let mut i = 0;
     let mut cache :HashSet<i32> = HashSet::new();
