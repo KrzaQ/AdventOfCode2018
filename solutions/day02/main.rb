0229260a30ecd02252b503465a651ab834b7e162
KrzaQ <krzaq@krzaq.cc>
2018-12-02 19:37:58 +0100

    Use Array.combination instead of rolling my own enumerator. Thanks @mikasjp.

--- solutions/day02/main.rb
+++ solutions/day02/main.rb
@@ -5,29 +5,18 @@ DATA = File.read('data.txt').split
 P1 = DATA.map do |s|
     s.each_codepoint
         .group_by{ |c| c }
-        .map{ |k,v| v.count }
+        .map{ |k, v| v.count }
         .select{ |v| [2,3].include? v }
 end
 
 PART1 = P1.count{ |v| v.include? 2 } * P1.count{ |v| v.include? 3 }
-
-def each_pair
-    Enumerator.new(DATA.size * (DATA.size-1)) do |out|
-        DATA.each do |l|
-            DATA.each do |r|
-                next if l == r
-                out << [l, r]
-            end
-        end
-    end
-end
 
 def count_dist l, r
     out = l.each_codepoint.zip(r.each_codepoint).select{ |l, r| l == r }
     out.size == l.size - 1 ? out.map(&:first).map(&:chr).join : nil
 end
 
-PART2 = each_pair
+PART2 = DATA.combination(2)
     .lazy
     .map{ |l, r| count_dist l, r }
     .select{ |v| v }
