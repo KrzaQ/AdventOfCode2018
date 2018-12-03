8f239ff2507ce66bbec8e1fb825dfe573a30d49a
KrzaQ <krzaq@krzaq.cc>
2018-12-03 06:25:15 +0100

    Day 03 done.

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ I'm using various languages to provide solutions.
 |:---:|:---|:---:|:---:|:---:|
 | 01 | [Chronal Calibration][day01] | [:heavy_check_mark:](solutions/day01/main.rb) | [:heavy_check_mark:](solutions/day01/rust/src/main.rs) | [:heavy_check_mark:](solutions/day01/main.d) |
 | 02 | [Inventory Management System][day02] | [:heavy_check_mark:](solutions/day02/main.rb) |  | [:heavy_check_mark:](solutions/day02/dlang/source/main.d) |
+| 03 | [No Matter How You Slice It][day03] | [:heavy_check_mark:](solutions/day03/main.rb) |  |  |
 
 [day01]: https://adventofcode.com/2018/day/1
 [day02]: https://adventofcode.com/2018/day/2
+[day03]: https://adventofcode.com/2018/day/3
 
 [kq]: https://dev.krzaq.cc
 
-Readme generated on 2018-12-02 20:31:05 +0100
+Readme generated on 2018-12-03 06:24:24 +0100
---
+++ solutions/day03/main.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+
+def arr_to_claim arr
+    ret = []
+    (arr[1]...(arr[1]+arr[3])).each do |x|
+        (arr[2]...(arr[2]+arr[4])).each do |y|
+            ret.push [[x, y], [1, [arr[0]]]]
+        end
+    end
+    return ret.to_h
+end
+
+$overlaps = {}
+DATA = File.read('data.txt')
+    .scan(/\#(\d+) \@ (\d+),(\d+): (\d+)x(\d+)/)
+    .map{ |n| n.map(&:to_i) }
+    .reduce({}) do |t, e|
+        if not $overlaps.include? e[0]
+            $overlaps[e[0]] = false
+        end
+    t.merge(arr_to_claim e) do |k, o, n|
+        r = [o.first + n.first, o.last + n.last]
+        r.last.each{ |x| $overlaps[x] = r.last.size > 1 }
+        r
+    end
+end
+
+PART1 = DATA.select{|k, v| v.first > 1 }.count
+PART2 = $overlaps.select{|k, v| v == false }.map{ |k, v| k }.join(", ")
+
+puts 'Part 1: %s' % PART1
+puts 'Part 2: %s' % PART2
