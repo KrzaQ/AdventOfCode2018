caf85d4caa178bb31155ece3e2eacbaf3f5fc8f9
KrzaQ <krzaq@krzaq.cc>
2018-12-05 07:33:34 +0100

    Day 05, the worst way.

--- README.md
+++ README.md
@@ -9,12 +9,14 @@ I'm using various languages to provide solutions.
 | 02 | [Inventory Management System][day02] | [:heavy_check_mark:](solutions/day02/main.rb) | [:heavy_check_mark:](solutions/day02/rust/src/main.rs) | [:heavy_check_mark:](solutions/day02/dlang/source/main.d) |
 | 03 | [No Matter How You Slice It][day03] | [:heavy_check_mark:](solutions/day03/main.rb) | [:heavy_check_mark:](solutions/day03/rust/src/main.rs) |  |
 | 04 | [Repose Record][day04] | [:heavy_check_mark:](solutions/day04/main.rb) |  |  |
+| 05 | [Alchemical Reduction][day05] | [:heavy_check_mark:](solutions/day05/main.rb) |  |  |
 
 [day01]: https://adventofcode.com/2018/day/1
 [day02]: https://adventofcode.com/2018/day/2
 [day03]: https://adventofcode.com/2018/day/3
 [day04]: https://adventofcode.com/2018/day/4
+[day05]: https://adventofcode.com/2018/day/5
 
 [kq]: https://dev.krzaq.cc
 
-Readme generated on 2018-12-04 06:32:30 +0100
+Readme generated on 2018-12-05 07:31:58 +0100
---
+++ solutions/day05/main.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/ruby
+
+DATA = File.read('data.txt').strip
+
+copy = [DATA.clone, true]
+
+def make_pass input, hint
+    input += "?"
+    hint = [hint-1, 0].max
+    n = hint
+    found = input.size
+
+    while n < input.size-1
+        e1 = input[n]
+        e2 = input[n+1]
+        if e1.upcase == e2.upcase and e1 != e2
+            found = [found, n].min
+            input[n..(n+1)] = '??'
+            n += 1
+        end
+        n += 1
+    end
+
+    orig_size = input.size - 1
+    input.gsub!('?','')
+    [input, input.size != orig_size, found]
+end
+
+def react str
+    arr = [str, true, 0]
+    loop do
+        arr = make_pass arr.first, arr.last
+        break unless arr[1]
+    end
+    arr.first
+end
+
+PART1 = react(DATA).size
+
+PART2 = ('a'..'z').map do |c|
+        x = react DATA.gsub(/[#{c}#{c.upcase}]/, '')
+        [c, x.size]
+    end
+    .to_a
+    .sort_by{ |k, v| v }
+    .first
+    .last
+
+puts 'Part 1: %s' % PART1
+puts 'Part 2: %s' % PART2
