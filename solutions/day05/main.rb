609b83a750cbbfcee67e334dc049019c9d11f45a
KrzaQ <krzaq@krzaq.cc>
2018-12-05 18:44:03 +0100

    O(n) day 5.

--- solutions/day05/main.rb
+++ solutions/day05/main.rb
@@ -2,36 +2,17 @@
 
 DATA = File.read('data.txt').strip
 
-copy = [DATA.clone, true]
-
-def make_pass input, hint
-    hint = [hint-1, 0].max
-    n = hint
-    found = input.size
-
-    while n < input.size-1
-        e1 = input[n]
-        e2 = input[n+1]
-        if e1.upcase == e2.upcase and e1 != e2
-            found = [found, n].min
-            input[n..(n+1)] = '??'
-            n += 1
-        end
-        n += 1
-    end
-
-    orig_size = input.size
-    input.gsub!('?','')
-    [input, input.size != orig_size, found]
-end
-
 def react str
-    arr = [str, true, 0]
-    loop do
-        arr = make_pass arr.first, arr.last
-        break unless arr[1]
-    end
-    arr.first
+    str.each_char
+        .each_with_object([' ']) do |e, s|
+            if s.last != e and s.last.upcase == e.upcase
+                s.pop
+            else
+                s.push e
+            end
+        end
+        .drop(1)
+        .join
 end
 
 PART1 = react(DATA).size
