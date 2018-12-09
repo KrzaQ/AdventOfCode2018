bc7111217bb106b4f8ec5f5e060b407616e080e0
KrzaQ <krzaq@krzaq.cc>
2018-12-09 07:20:26 +0100

    Remove evil.

--- solutions/day09/main.cpp
+++ solutions/day09/main.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <algorithm>
 #include <list>
-using namespace std;
 
 using result_t = uint64_t;
 
@@ -11,30 +10,30 @@ result_t solve(int players, int last);
 
 int main()
 {
-    std::cout << "Part 1: " << solve(403, 71920) << std::endl;
-    std::cout << "Part 2: " << solve(403, 7192000) << std::endl;
+    std::cout << "Part 1: " << solve(403, 71920) << '\n';
+    std::cout << "Part 2: " << solve(403, 7192000) << '\n';
 }
 
 result_t solve(int players, int last)
 {
-    list<int> arr{0};
+    std::list<int> arr{0};
     auto cur_it = arr.begin();
 
-    vector<result_t> scores(players, 0);
+    std::vector<result_t> scores(players, 0);
 
-    auto cycle_next = [&](auto& it, int n){
+    auto cycle_next = [&](auto& it, int n) {
         for(int i = 0; i < n; i++) {
             if(it == arr.end())
                 it = arr.begin();
-            advance(it, 1);
+            std::advance(it, 1);
         }
     };
 
-    auto cycle_prev = [&](auto& it, int n){
+    auto cycle_prev = [&](auto& it, int n) {
         for(int i = 0; i < n; i++) {
             if(it == arr.begin())
                 it = arr.end();
-            advance(it, -1);
+            std::advance(it, -1);
         }
     };
 
